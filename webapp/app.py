d297bede65418590f853dd7a0981e5f202c807ef
dms486 <dms486@cornell.edu>
2020-05-04 19:23:09 -0400

    better app

--- webapp/app.py
+++ webapp/app.py
@@ -7,18 +7,20 @@ msg_dict = {}
 @app.route('/', methods=['POST', 'GET'])
 def message_form_post():
     if request.method == 'GET':
-        return render_template('page.html')
+        return render_template('html_example.html')
     
     message = request.form['message']
-    msg_dict[message] = message
+    print(message)
+    #msg_dict[message] = message
 
-    templateData = {
-            'msg_dict' : msg_dict
-    }
+    #templateData = {
+    #        'msg_dict' : msg_dict
+    #}
 
     #send message to the other server here, maybe via FIFO?
 
-    return render_template('page.html', **templateData)
+    #return render_template('page.html', **templateData)
+    return render_template('html_example.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
