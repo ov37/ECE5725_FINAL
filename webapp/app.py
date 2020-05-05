0049eee49d82c5fd2a9c46031dcb57e7752e13a2
ov37 <ov37@cornell.edu>
2020-05-05 21:55:46 +0100

    integrated mqtt_publish into webapp

--- webapp/app.py
+++ webapp/app.py
@@ -1,5 +1,21 @@
 from flask import Flask, request, render_template
+import paho.mqtt.publish as publish
+import json
 
+MQTT_API_KEY = 'SESPS20JB3HS9FNO'
+WRITE_API_KEY = '1TIH3KFRFKEWYA8L'
+channelID = "1040309"
+field_number = '1'
+
+
+topic = "channels/" + channelID + "/publish/fields/field" + field_number + "/" + WRITE_API_KEY
+
+#while 1:
+#    x = raw_input("Send message: ")
+#    payload = str(x)
+#    print "X is: " + payload
+#    r = publish.single(topic, payload, hostname = "mqtt.thingspeak.com", port = 1883, auth = {'username':"user", 'password':MQTT_API_KEY})
+ 
 app = Flask(__name__)
 
 msg_dict = {}
@@ -11,6 +27,9 @@ def message_form_post():
     
     message = request.form['message']
     print(message)
+    payload = message
+    r = publish.single(topic, payload, hostname = "mqtt.thingspeak.com", port = 1883, auth = {'username':"user", 'password':MQTT_API_KEY})
+
     #msg_dict[message] = message
 
     #templateData = {
