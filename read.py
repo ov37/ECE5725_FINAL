af10dd6e2b5ad3317094e81c7f570daf058ff48a
Owen Valdescruz <42687533+ov37@users.noreply.github.com>
2020-05-21 15:32:56 -0400

    Delete read.py

--- read.py
+++
@@ -1,9 +0,0 @@
-import requests
-import json
-
-
-#payload = {'api_key': API_KEY, 'field1': str(x)}
-#print "X is: " + x
-r = requests.get('https://api.thingspeak.com/channels/1040309/feeds.json?api_key=2D4RRJVU70QBL1YS&start=2020-04-17%2016:15:00&timezone=America%2FNew_York')
-#print (r.json())["feeds"][5]["field1"]
-print r.json()["feeds"]
