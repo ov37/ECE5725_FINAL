e682320ad6a9f999a182ad9f0eed38edd3206fe6
Owen Valdescruz <42687533+ov37@users.noreply.github.com>
2020-05-21 15:33:32 -0400

    Delete thingspeak.py

--- thingspeak.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-API_KEY = "1TIH3KFRFKEWYA8L"
-x = raw_input("Send message: ")
-payload = {'api_key': API_KEY, 'field1': str(x)}
-print "X is: " + x
-r = requests.post('https://api.thingspeak.com/update', params=payload)
-print r
